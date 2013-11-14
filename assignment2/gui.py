423bf1171b4ec28f3f0d5af0bb4212d16f5fe68c
Aaron Karper <akarper@students.unibe.ch>
2013-11-14 01:21:04 +0100

    Point selection works

--- assignment2/gui.py
+++ assignment2/gui.py
@@ -1,23 +1,41 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+from matplotlib.patches import CirclePolygon
 
 
-def onclick(event):
-    print 'x=%f, y=%f %s'%(event.xdata, event.ydata, event.inaxes)
+class StereoVision(object):
+    def __init__(self, l, r):
+        self.imgl = mpimg.imread(l)
+        self.imgr = mpimg.imread(r)
+        self.fig = plt.figure()
+
+    def onclick(self, event):
+        r = self.resolve(event.inaxes)
+        print 'x=%f, y=%f %s'%(event.xdata, event.ydata, r)
+
+        circ = CirclePolygon((event.xdata, event.ydata), 5, color='r')
+        if r == 'r':
+            self.ax1.add_patch(circ)
+        else:
+            self.ax2.add_patch(circ)
+        self.fig.canvas.draw()
+
+    def resolve(self, ax):
+        return "l" if ax == self.ax2 else "r"
+
+    def plot(self):
+        self.ax1 = self.fig.add_subplot(1, 2, 0)
+        plt.imshow(self.imgl, cmap='gray')
+
+        self.ax2 = self.fig.add_subplot(1, 2, 1)
+        plt.imshow(self.imgr, cmap='gray')
+
+        self.ax1.figure.canvas.mpl_connect('button_press_event', self.onclick)
+
+        plt.show()
 
 if __name__ == '__main__':
     import sys
-    fig = plt.figure()
-
-    imgl = mpimg.imread(sys.argv[1])
-    imgr = mpimg.imread(sys.argv[2])
-
-    ax1 = fig.add_subplot(1, 2, 0, axes=None)
-    plt.imshow(imgl)
-    ax1.figure.canvas.mpl_connect('button_press_event', onclick)
-
-    ax2 = fig.add_subplot(1, 2, 1, frame_on=False)
-    plt.imshow(imgr)
-
-    plt.show()
+    v = StereoVision(sys.argv[1], sys.argv[2])
+    v.plot()
