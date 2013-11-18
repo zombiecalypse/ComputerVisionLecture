Fixed fundamental matrix calculation

assignment2/fun.py
@@ -1,3 +1,4 @@
+# coding=utf8
 import numpy as np
 import scipy as sp
 import scipy.linalg as l
@@ -13,49 +14,83 @@ log = logging.getLogger(__name__)
 #
 def produce_row(p1, p2):
     """Produces restriction row for 2d points p1, p2."""
-    p1 = np.array(p1)
-    p2 = np.array(p2)
-    p1 = p1/l.norm(p1)
-    p2 = p2/l.norm(p2)
-    return np.array([p1[0]*p2[0], p1[0]*p2[1], p1[0],
-                    p1[1]*p2[0], p1[1]*p2[1], p1[1],
-                    p2[0], p2[1], 1]) # trimmed to get unique solution
+    x_p, y_p = p1[0], p1[1]
+    x,   y   = p2[0], p2[1]
+    return np.array([
+            x_p*x, x_p*y, x_p,
+            y_p*x, y_p*y, y_p,
+            x, y, 1])
+
+# mean(T⁻¹ X) = 0
+# std(T⁻¹ X)² = 2
+def normalized(P):
+    mu = P.mean(axis=0)
+    delta = P - mu
+    sig = np.sqrt((delta**2).sum(axis=1)).mean()
+    log.debug("mu: %s sig: %s", mu, sig)
+    T = np.eye(3,3)
+    # Normalize for std deviation
+    sig_target = 2/sig
+    T[0,0] *= sig_target
+    T[1,1] *= sig_target
+    # Normalize for mean
+    T[0:2, 2] = -mu*sig_target
+    Pp = np.hstack([P, np.array([[1]]*P.shape[0])])
+    Q = l.solve(T, Pp.T).T
+
+    # Check functionality: OK
+    log.debug("%s vs %s", T.shape, Pp.shape)
+    log.debug("%s", np.dot(T, np.array([mu[0], mu[1], 1])))
+    P_p = np.dot(T, Pp.T).T
+    mu_p = P_p.mean(axis=0)
+    delta_p = P_p - mu_p
+    sig_p = np.sqrt((delta_p**2).sum(axis=1)).mean()
+    log.debug("mu': %s sig: %s", mu_p, sig_p)
+    return Q, T
 
 def produce_matrix(P1, P2):
     """Produces a matrix that can be solved for F"""
-    log.debug("Prod matrix:")
-    log.debug(len(P1))
-    log.debug(P1)
-    log.debug(len(P2))
-    log.debug(P2)
     assert len(P1) == len(P2)
     assert len(P1) > 7
-    return np.vstack([produce_row(p1, p2) for p1, p2 in zip(P1, P2)])
+    P1 = np.array(P1)
+    P2 = np.array(P2)
+    P1, T1 = normalized(P1)
+    P2, T2 = normalized(P2)
+    return np.vstack([produce_row(p1, p2) for p1, p2 in zip(P1, P2)]), T1, T2
 
 def rank(A, eps=1e-12):
     u, s, vh = np.linalg.svd(A)
     return len([x for x in s if abs(x) > eps])
 
+def enforce_rank2(M):
+    u, s, vh = np.linalg.svd(M)
+    s[2] = 0
+    return np.dot(u, np.dot(np.diag(s), vh))
+
+def dot(*args):
+    return reduce(np.dot, args)
+
 def get_F(P1, P2):
-    log.debug('produce matrix')
-    A = produce_matrix(P1, P2)
-    log.debug('A:\n%s', str(A))
-    f = l.pinv(A)[8]
-    log.debug(str(f))
-    f = f/l.norm(f)
-    # Ensure F does have rank 2:
-    # f[8] so that
-    #   0 = c f[0] + d f[3] - f[6]
-    #   0 = c f[1] + d f[4] - f[7]
-    #   0 = c f[2] + d f[5] - f[8]
-    log.info(f.shape)
-    f.shape = (3, 3)
+    A, T1, T2 = produce_matrix(P1, P2)
+    u, s, vh = np.linalg.svd(A)
+    f = vh[8]
+    f.shape = (3,3)
+    f = enforce_rank2(f)
+    f = dot(T2.T, f, T1)
+    # Check functionality: OK
+    log.debug('rank: %s', rank(f))
+    for i, (p1, p2) in enumerate(zip(P1, P2)):
+        p1 = dot(T1, np.append(p1, 1))
+        p2 = dot(T2, np.append(p2, 1))
+        # Product should be 1:
+        # p1 perp to p2, except for last component, which is 1 for both
+        log.debug('%8s: %s', i, dot(p2.T, f, p1)-1)
     return f
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     P1 = ((103.96451612903223, 119.24193548387109),
-          (4.041935483870958, 253.22903225806465),
+          (4.041935483870958,  253.22903225806465),
           (117.59032258064514, 305.46129032258074),
           (312.89354838709676, 210.08064516129036),
           (358.31290322580645, 332.71290322580654),

assignment2/gui.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+import pylab
 from matplotlib.patches import CirclePolygon
 
 import logging
@@ -9,7 +10,14 @@ logging.basicConfig(level=logging.INFO)
 
 import fun
 fun.log.level = logging.INFO
+from random import shuffle
 
+def cmap_as_list(cmap,n=20):
+    if type(cmap) == str:
+        cmap = pylab.get_cmap(cmap)
+    p = np.linspace(0, 1, n)
+    cols = cmap(p)
+    return cols
 
 class StereoVision(object):
     def __init__(self, l, r):
@@ -18,32 +26,41 @@ class StereoVision(object):
         self.fig = plt.figure()
         self.pointl = []
         self.pointr = []
+        self.F = None
+        self.cmap = cmap_as_list('hsv')
+        shuffle(self.cmap)
 
     def onclick(self, event):
         r = self.resolve(event.inaxes)
         try:
             log.debug('x=%f, y=%f %s'%(event.xdata, event.ydata, r))
 
-            circ = CirclePolygon((event.xdata, event.ydata), 5, color='r',
+            circ = CirclePolygon((event.xdata, event.ydata), 5,
                                  alpha=0.75)
             if r == 'r':
+                col = self.cmap[len(self.pointr)]
+                circ.set_color(col)
                 self.ax1.add_patch(circ)
                 self.pointr.append((event.xdata, event.ydata))
             else:
+                col = self.cmap[len(self.pointl)]
+                circ.set_color(col)
                 self.ax2.add_patch(circ)
                 self.pointl.append((event.xdata, event.ydata))
             self.fig.canvas.draw()
 
-            if len(self.pointr) > 8 and len(self.pointl) > 8:
-                log.info("Calculate F now...")
-                F = self.calculateF()
-                log.info("done...")
-                log.info("F: %s", F)
+            if len(self.pointr) > 8 and len(self.pointl) > 8 and self.F is None:
+                self.calculateF()
         except:
             pass
 
     def calculateF(self):
-        return fun.get_F(tuple(self.pointl[:9]), tuple(self.pointr[:9]))
+        self.F = 'calculate'
+        log.info("Calculate F now...")
+        minlen = min(self.pointl.shape[0], self.pointr.shape[0])
+        self.F = fun.get_F(tuple(self.pointl[:minlen]), tuple(self.pointr[:minlen]))
+        log.info("done...")
+        log.info("F: %s", self.F)
 
     def resolve(self, ax):
         return "l" if ax == self.ax2 else "r"