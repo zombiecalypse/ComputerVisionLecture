ca9dbdc8fb4753e1420651e1ccda5cc544461361
Aaron Karper <akarper@students.unibe.ch>
2013-11-18 22:48:19 +0100

    plotting epipolar line sux

--- assignment2/gui.py
+++ assignment2/gui.py
@@ -3,6 +3,8 @@ from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 import pylab
 from matplotlib.patches import CirclePolygon
+from matplotlib.lines import Line2D
+import scipy.linalg as l
 
 import logging
 log = logging.getLogger(__name__)
@@ -10,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 
 import fun
 fun.log.level = logging.INFO
-from random import shuffle
+from random import random
 
 def cmap_as_list(cmap,n=20):
     if type(cmap) == str:
@@ -24,40 +26,61 @@ class StereoVision(object):
         self.imgl = mpimg.imread(l)
         self.imgr = mpimg.imread(r)
         self.fig = plt.figure()
-        self.pointl = []
-        self.pointr = []
+        self.pointl = list(fun.P1)
+        self.pointr = list(fun.P2)
+        self.calculateF()
+        #self.pointl = []
+        #self.pointr= []
         self.F = None
-        self.cmap = cmap_as_list('hsv')
-        shuffle(self.cmap)
+        self.cmap = sorted(cmap_as_list('hsv'), key=lambda x:random())
+
+    def plot_point(self, x, y, r):
+        log.debug('x=%f, y=%f %s'%(x,y, r))
+
+        circ = CirclePolygon((x,y), 5,
+                             alpha=0.75)
+        if r == 'r':
+            col = self.cmap[len(self.pointr)%20]
+            circ.set_color(col)
+            self.ax1.add_patch(circ)
+            self.pointr.append((x, y))
+        else:
+            col = self.cmap[len(self.pointl)%20]
+            circ.set_color(col)
+            self.ax2.add_patch(circ)
+            self.pointl.append((x, y))
+        if isinstance(self.F, np.ndarray):
+            self.draw_line(x, y, r, col)
+        self.fig.canvas.draw()
+
+    def draw_line(self, x, y, r, col):
+        assert isinstance(self.F, np.ndarray)
+        # calculate second point p2 on epipolar line
+        # p F x = 0
+        p = np.array([x, y, 1])
+        n = np.dot(self.F, p)
+        log.info('n: %s', n)
+        p1 = (-n[2]-self.imgl.shape[0])/n[1]
+        p2 = (-n[2])/n[1]
+        log.info('p1: %s p2: %s', p1, p2)
+        # define a line in other image
+        self.ax1.plot(p1/(p1+p2), (p2-p1)/(p1+p2))
+        self.ax1.axis('image')
 
     def onclick(self, event):
         r = self.resolve(event.inaxes)
         try:
-            log.debug('x=%f, y=%f %s'%(event.xdata, event.ydata, r))
+            self.plot_point(event.xdata, event.ydata, r)
 
-            circ = CirclePolygon((event.xdata, event.ydata), 5,
-                                 alpha=0.75)
-            if r == 'r':
-                col = self.cmap[len(self.pointr)]
-                circ.set_color(col)
-                self.ax1.add_patch(circ)
-                self.pointr.append((event.xdata, event.ydata))
-            else:
-                col = self.cmap[len(self.pointl)]
-                circ.set_color(col)
-                self.ax2.add_patch(circ)
-                self.pointl.append((event.xdata, event.ydata))
-            self.fig.canvas.draw()
-
-            if len(self.pointr) > 8 and len(self.pointl) > 8 and self.F is None:
+            if len(self.pointr) > 7 and len(self.pointl) > 7 and self.F is None:
                 self.calculateF()
-        except:
-            pass
+        except Exception as e:
+            log.error(e)
 
     def calculateF(self):
         self.F = 'calculate'
         log.info("Calculate F now...")
-        minlen = min(self.pointl.shape[0], self.pointr.shape[0])
+        minlen = min(len(self.pointl), len(self.pointr))
         self.F = fun.get_F(tuple(self.pointl[:minlen]), tuple(self.pointr[:minlen]))
         log.info("done...")
         log.info("F: %s", self.F)
